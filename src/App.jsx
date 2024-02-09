8be0d07dea7fb75b828f1def6a15b9392f6b85e6
Dev. Ralph <raphaelakpor00@gmail.com>
2024-02-09 20:30:54 +0100

    Code Modified

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,12 @@ function App() {
   return (
     <>
       <Wrapper />
-        <Header />
-        <Hero />
+      <Header />
+      <Hero />
       <FirstSection />
       <SecondSection />
       <ThirdSection />
       <Footer />
-
-      {/* <div class="attribution">
-      Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-        >Frontend Mentor</a
-      >. Coded by <a href="#">Your Name Here</a>.
-    </div> */}
     </>
   );
 }
